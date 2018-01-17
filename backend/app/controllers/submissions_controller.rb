844d6413ce1465b455f3698dd5175c0bca76bba3
Eugene Doan <eugene@adhocteam.us>
2018-01-17 14:37:37 -0500

    Added show action.

--- backend/app/controllers/submissions_controller.rb
+++ backend/app/controllers/submissions_controller.rb
@@ -1,5 +1,12 @@
 class SubmissionsController < ApplicationController
   def index
+    @submissions = Submission.all
+    render json: @submissions
+  end
+
+  def show
+    @submission = Submission.find(params[:id])
+    render json: @submission
   end
 
   def create
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :submissions, only: [:index, :create, :update]
+  resources :submissions, only: [:index, :show, :create, :update]
 end
