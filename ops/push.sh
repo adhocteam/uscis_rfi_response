712fc633359a5cef5b756fcf11a64e0ede7424cf
Ryan Nagle <rmnagle@gmail.com>
2018-01-17 17:25:46 -0600

    tag most recent push with latest while we're at it

--- ops/push.sh
+++ ops/push.sh
@@ -19,6 +19,8 @@ $(aws ecr get-login --no-include-email --region=$REGION)
 
 # Tag the freshly build image with ECR repo
 docker tag $REPO:$TAG $FULLREPO:$TAG
+docker tag $REPO:$TAG $FULLREPO:latest
 
 # Push the ECR tagged image to ECR repo
 docker push $FULLREPO:$TAG
+docker push $FULLREPO:latest
