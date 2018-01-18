48602d8e0e459af1fbab328d54001c4c26198b89
Eugene Doan <eugene@adhocteam.us>
2018-01-18 15:30:07 -0500

    Fixed Submission model.

--- backend/app/controllers/submissions_controller.rb
+++ backend/app/controllers/submissions_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SubmissionsController < ApplicationController
   def index
     @submissions = Submission.all
--- backend/app/models/submission.rb
+++ backend/app/models/submission.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Submission < ApplicationRecord
-  belongs_to :user foreign_key: :user_uuid, primary_key: uuid
+  belongs_to :user, foreign_key: :user_uuid, primary_key: :uuid
   enum status: [ :requested, :submitted, :approved, :rejected ]
 end
