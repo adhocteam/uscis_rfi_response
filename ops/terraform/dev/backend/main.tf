31ebb95371b67cd23ef6df3b667a21acac123aa9
Ryan Nagle <rmnagle@gmail.com>
2018-01-18 09:21:54 -0600

    s3 bucket and kms key for config vars and secrets

--- ops/terraform/dev/backend/main.tf
+++ ops/terraform/dev/backend/main.tf
@@ -39,3 +39,55 @@ module "uscis_backend" {
 
   service_version    = "${var.service_version}"
 }
+
+# For env variable and secret storage
+resource "aws_s3_bucket" "uscis_backend_config_vars" {
+  bucket = "uscis-backend-config-vars"
+  acl    = "private"
+}
+
+resource "aws_kms_key" "uscis_backend_config_vars" {
+  description             = "USCIS Backend configuration variables and secrets"
+  deletion_window_in_days = 7
+}
+
+resource "aws_kms_alias" "uscis_backend_config_vars" {
+  name          = "alias/uscis_backend"
+  target_key_id = "${aws_kms_key.uscis_backend_config_vars.key_id}"
+}
+
+resource "aws_s3_bucket_policy" "uscis_backend_config_vars" {
+  bucket = "${aws_s3_bucket.uscis_backend_config_vars.id}"
+  policy =<<POLICY
+{
+   "Version": "2012-10-17",
+   "Id": "PutObjPolicy",
+   "Statement": [
+         {
+              "Sid": "DenyIncorrectEncryptionHeader",
+              "Effect": "Deny",
+              "Principal": "*",
+              "Action": "s3:PutObject",
+              "Resource": "${aws_s3_bucket.uscis_backend_config_vars.arn}/*",
+              "Condition": {
+                      "StringNotEquals": {
+                             "s3:x-amz-server-side-encryption": "aws:kms"
+                       }
+              }
+         },
+         {
+              "Sid": "DenyUnEncryptedObjectUploads",
+              "Effect": "Deny",
+              "Principal": "*",
+              "Action": "s3:PutObject",
+              "Resource": "${aws_s3_bucket.uscis_backend_config_vars.arn}/*",
+              "Condition": {
+                      "Null": {
+                             "s3:x-amz-server-side-encryption": true
+                      }
+             }
+         }
+   ]
+}
+POLICY
+}
