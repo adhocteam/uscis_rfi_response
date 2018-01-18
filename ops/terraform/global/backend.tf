6c7e0cb4b5815aa7ec525ebac27c04c96ec19de8
Ryan Nagle <rmnagle@gmail.com>
2018-01-18 11:18:30 -0600

    move the config var bucket definitions out of dev, into global

---
+++ ops/terraform/global/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "uscis-tf-state"
+    key            = "global/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "uscis-tf-table"
+    # TODO(rnagle): encrypt with KMS
+    #encrypt        = "1"
+    #kms_key_id     = ""
+  }
+}
