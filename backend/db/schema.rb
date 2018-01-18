ec4bd32566986b174ba17f7bc0b49e064d81a220
Eugene Doan <eugene@adhocteam.us>
2018-01-18 15:29:07 -0500

    Schema.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180117191046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "submissions", force: :cascade do |t|
+    t.string "user_uuid"
+    t.datetime "timestamp"
+    t.string "uri"
+    t.integer "status"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "uuid"
+    t.string "name"
+    t.string "email"
+    t.string "street1"
+    t.string "street2"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.date "dob"
+    t.integer "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
