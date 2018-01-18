0d8e5912f840b54af80ea268881f480260e2c514
Eugene Doan <eugene@adhocteam.us>
2018-01-18 14:31:25 -0500

    Removed unnecessary primary key definition in migration.

--- backend/db/migrate/20180117191046_create_submissions.rb
+++ backend/db/migrate/20180117191046_create_submissions.rb
@@ -2,7 +2,6 @@ class CreateSubmissions < ActiveRecord::Migration[5.1]
   def change
     create_table :submissions do |t|
       t.string :user_uuid
-      t.primary_key :id
       t.datetime :timestamp
       t.string :uri
       t.integer :status
