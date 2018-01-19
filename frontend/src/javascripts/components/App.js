b16ae1dbbbbe31e9208ebc11659ff8643672798c
Louis Fettet <louisfettet@gmail.com>
2018-01-18 21:09:04 -0500

    Initial attempt at including design system. React components went swimmingly.  Styles, images, and fonts did not.

--- frontend/src/javascripts/components/App.js
+++ frontend/src/javascripts/components/App.js
@@ -5,10 +5,26 @@ import UploadPage from "./UploadPage";
 export default function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={UploadPage} />
-        <Redirect to="/" />
-      </Switch>
+      <div>
+        <div className="ds-u-fill--primary-alt">
+          <div className="ds-l-container ds-u-md-padding-top--1">
+            <div className="ds-l-row">
+              <div className="ds-l-col">
+                <h1
+                  id="main"
+                  className="ds-u-serif ds-u-font-size--h2 ds-u-md-font-size--h1 ds-u-font-weight--normal ds-u-margin-top--3 ds-u-margin-bottom--3"
+                >
+                  USCIS Uploader Front-End
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Switch>
+          <Route path="/" exact component={UploadPage} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
