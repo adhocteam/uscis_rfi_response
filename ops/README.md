2fc5c650e22b2be29b08cbdb290d58f4cd70268e
Ryan Nagle <rmnagle@gmail.com>
2018-01-19 11:07:56 -0600

    Terraform configs and ansible playbook for a jenkins server (#28)
    * terraform configs and ansible playbook for a jenkins server
    
    * allow jenkins to spawn new docker containers for builds, add method to build custom jenkins image w/ required tooling
    
    * jenkins iam role, adjustments to jenkins/docker config to get builds working
    
    * make sure the jenkins container restarts
    
    * add public ip to outputs for jenkins tf config
    
    * update ops readme, add notes on jenkins configs
    
    * allow jenkins instance to login to ecr to push docker images
    
    * some work on jenkins image and iam permissions for allowing it to deploy for us
    
    * a few tweaks to the deployment process to make it safer for jenkins to run

--- ops/README.md
+++ ops/README.md
@@ -1,3 +1,44 @@
+# Terraform configs
+
+See `ops/terraform/README.md`
+
+# Jenkins
+
+## Terraform
+
+The included configs will provision:
+
+- A separate VPC for housing Jenkins-related resources
+- An ec2 instance to run Jenkins
+- 2 EBS volumes
+    - One for Docker (storage of images and writeable layers)
+    - One for persistence of Jenkins' home directory
+
+To bring up Jenkins infrastructure:
+
+```
+cd ops/terraform/jenkins
+terraform apply
+```
+
+## Ansible playbook
+
+The included Ansible playbook:
+
+- Installs Docker and Nginx on the ec2 instance brought up in the previous step
+- Configures the EBS volumes attached to the instance
+- Bakes a custom Jenkins image (based on the official [Jenkins Docker image](https://hub.docker.com/r/jenkins/jenkins/)) with our requirements
+- Starts (or restarts) the Jenkins container and Nginx
+
+To use the playbook, grab the public IP address of the ec2 instance from the previous step, run the following commands:
+
+```
+cd ops/playbooks
+ansible-playbook -i "<public_ip_address>," jenkins.yml
+```
+
+# Build and deploy scripts
+
 Build docker image for backend app:
 
 ```
@@ -5,9 +46,9 @@ cd ops
 ./build.sh <env> <version>
 ```
 
-This produces the image: `uscis-backend:latest`
+This produces the image: `uscis-backend:<env>-<version>`
 
-The <version> arg can be anything, but should probably match a git commit or tag.
+If no <version> is supplied, the current git commit is used.
 
 To push to the Elastic Container Registry:
 
---
+++ ops/terraform/jenkins/main.tf
@@ -0,0 +1,171 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+module "uscis_shared_vpc" {
+  source = "../modules/vpc"
+  vpc_name = "uscis-shared-vpc"
+  az_count = 1
+}
+
+data "aws_ami" "aws_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  owners = ["amazon"]
+}
+
+data "aws_kms_alias" "uscis_backend" {
+  name = "alias/uscis_backend"
+}
+
+resource "aws_security_group" "ssh" {
+  name        = "ssh"
+  description = "Allow SSH connections"
+  vpc_id      = "${module.uscis_shared_vpc.vpc_id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "http" {
+  name        = "http"
+  description = "Allow HTTP connections"
+  vpc_id      = "${module.uscis_shared_vpc.vpc_id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_instance_profile" "uscis_jenkins_profile" {
+  name  = "tf-uscis-jenkins-profile"
+  role = "${aws_iam_role.uscis_jenkins_role.name}"
+}
+
+resource "aws_iam_role" "uscis_jenkins_role" {
+  name = "tf-uscis-jenkins-role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+data "template_file" "uscis_jenkins_policy" {
+  template = "${file("${path.module}/templates/jenkins.json")}"
+
+  vars {
+    account_id = "${data.aws_caller_identity.current.account_id}"
+  }
+}
+
+resource "aws_iam_role_policy" "uscis_jenkins_policy" {
+    name   = "tf-uscis-jenkins-policy"
+    role   = "${aws_iam_role.uscis_jenkins_role.id}"
+    policy = "${data.template_file.uscis_jenkins_policy.rendered}"
+}
+
+resource "aws_instance" "jenkins" {
+  ami                         = "${data.aws_ami.aws_linux.id}"
+  instance_type               = "t2.small"
+  key_name                    = "ecs"
+  vpc_security_group_ids      = ["${aws_security_group.ssh.id}", "${aws_security_group.http.id}"]
+  subnet_id                   = "${element(module.uscis_shared_vpc.subnets_ids, 0)}"
+  associate_public_ip_address = true
+  iam_instance_profile        = "tf-uscis-jenkins-profile"
+
+  root_block_device {
+    volume_type = "standard"
+    volume_size = 30
+  }
+
+  tags {
+    Name = "uscis-jenkins"
+  }
+}
+
+resource "aws_ebs_volume" "docker_home" {
+  availability_zone = "${aws_instance.jenkins.availability_zone}"
+  type       = "gp2"
+  size       = 100
+  encrypted  = true
+  kms_key_id = "arn:aws:kms:us-east-1:${data.aws_caller_identity.current.account_id}:key/${data.aws_kms_alias.uscis_backend.target_key_id}"
+
+  tags {
+    Name = "uscis-docker-home"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_one" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.docker_home.id}"
+  instance_id = "${aws_instance.jenkins.id}"
+}
+
+resource "aws_ebs_volume" "jenkins_home" {
+  availability_zone = "${aws_instance.jenkins.availability_zone}"
+  type       = "gp2"
+  size       = 100
+  encrypted  = true
+  kms_key_id = "arn:aws:kms:us-east-1:${data.aws_caller_identity.current.account_id}:key/${data.aws_kms_alias.uscis_backend.target_key_id}"
+
+  tags {
+    Name = "uscis-jenkins-home"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_two" {
+  device_name = "/dev/sdg"
+  volume_id   = "${aws_ebs_volume.jenkins_home.id}"
+  instance_id = "${aws_instance.jenkins.id}"
+}
