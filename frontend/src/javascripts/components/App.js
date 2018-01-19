9c9c54805295f9b48db6e416102fc129f4049db8
Brian King <kingishb@gmail.com>
2018-01-19 18:20:27 +0000

    Private Route and Login Components

--- frontend/src/javascripts/components/App.js
+++ frontend/src/javascripts/components/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import UploadPage from "./UploadPage";
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
+
+const Protected = () => <h3>Protected</h3>;
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={UploadPage} />
+        <Route path="/login" exact component={Login} />
+        <PrivateRoute path="/private" exact component={Protected} />
         <Redirect to="/" />
       </Switch>
     </BrowserRouter>
---
+++ frontend/src/javascripts/components/Login.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Auth } from "./App";
+import validToken from "../services/AuthService";
+class Login extends React.Component {
+  state = {
+    redirectToReferrer: false,
+    username: "",
+    password: ""
+  };
+  login = e => {
+    e.preventDefault();
+    sessionStorage.setItem("token", "fhq412p89fqfp48h");
+    this.setState(() => ({
+      redirectToReferrer: true
+    }));
+  };
+  render() {
+    if (validToken()) {
+      this.props.history.push("/private");
+    }
+
+    return (
+      <div className="Login">
+        <h2> Login </h2>
+        <p> Please enter your email and password </p>
+        <p>You must log in to view the page</p>
+        <form onSubmit={this.login}>
+          <label>
+            Username
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={e => this.setState({ username: e.target.value })}
+            />
+          </label>
+          <br />
+          <label>
+            Password
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+          </label>
+          <br />
+          <input type="submit" value="Log In" />
+        </form>
+      </div>
+    );
+  }
+}
+export default Login;
