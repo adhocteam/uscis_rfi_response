155890c98c746a2a0580b5c95f01723ed581ad47
Eugene Doan <eugene@adhocteam.us>
2018-01-19 12:25:20 -0500

    Added UUID to submission.

--- backend/app/models/submission.rb
+++ backend/app/models/submission.rb
@@ -3,4 +3,6 @@
 class Submission < ApplicationRecord
   belongs_to :user
   enum status: [ :requested, :submitted, :approved, :rejected ]
+
+  after_initialize { self.uuid = SecureRandom.uuid }
 end
