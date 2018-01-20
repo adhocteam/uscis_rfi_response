fbb769bf206f4f1bd72a0196b393face7ee17571
Eugene Doan <eugene@adhocteam.us>
2018-01-20 01:32:21 -0500

    Updated controller and request spec.

--- backend/app/controllers/submissions_controller.rb
+++ backend/app/controllers/submissions_controller.rb
@@ -8,7 +8,7 @@ class SubmissionsController < ApplicationController
 
   def show
     @submission = Submission.find(params[:id])
-    render json: @submission, include: :user
+    render json: @submission, include: :customer
   end
 
   def create
