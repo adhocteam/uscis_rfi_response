aaace9f494b762767a317b434856b8ef23fa8490
Brian King <kingishb@gmail.com>
2018-01-22 16:07:51 -0500

    Merged ops/readme with master.

--- backend/db/migrate/20180120062818_create_submissions.rb
+++ backend/db/migrate/20180120062818_create_submissions.rb
@@ -1,11 +1,11 @@
 class CreateSubmissions < ActiveRecord::Migration[5.1]
   def change
-    create_table :submissions do |t|
+    create_table :submissions, id: :uuid do |t|
       t.datetime :timestamp
       t.string :uri
       t.integer :status
       t.text :notes
-      t.references :user, foreign_key: true
+      t.references :customer, foreign_key: true
 
       t.timestamps
     end
