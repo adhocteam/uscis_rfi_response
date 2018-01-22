ff820d94d17a56e30ddb85a65d3c332ba163174b
Bill Mill <bill@billmill.org>
2018-01-22 14:00:24 -0500

    re-add presigned_url test

--- backend/spec/requests/submissions_request_spec.rb
+++ backend/spec/requests/submissions_request_spec.rb
@@ -96,4 +96,23 @@ RSpec.describe SubmissionsController, type: :request do
       expect(body['status']).to eq('denied')
     end
   end
+  describe 'post #presigned_url' do
+    it 'returns http success' do
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+      # to generate the image, I found a tiny png world map, and did:
+      # base64 < world_tiny.png
+      post '/submissions/presigned_url', params: <<~POST, headers: headers
+        {
+          "user_id": "e3ca6d96454e4a508a677e9e6c3dc3e3",
+          "image_name": "my cat.jpg",
+          "image_type": "image/png"
+        }
+      POST
+      expect(response).to have_http_status(:success)
+      url = 'https://uscis-rfds.s3.us-stubbed-1.amazonaws.com/e3ca6d96454e4a508a677e9e6c3dc3e3-my%20cat.jpg'
+      body = JSON.parse(response.body)
+      expect(body['status']).to eq 'ok'
+      expect(body['signedUrl']).to match url
+    end
+  end
 end
