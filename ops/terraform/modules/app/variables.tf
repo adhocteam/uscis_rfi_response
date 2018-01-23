b3753501251d6838d4fbe146bc820657eba57754
Ryan Nagle <rmnagle@gmail.com>
2018-01-23 16:13:33 -0600

    configure https for the app tf module

--- ops/terraform/modules/app/variables.tf
+++ ops/terraform/modules/app/variables.tf
@@ -63,3 +63,8 @@ variable "task_count" {
   description = "Desired number ECS tasks to run"
   default     = 2
 }
+
+variable "ssl_cert_id" {
+  description = "ACM cert arn to use with the Jenkins ELB"
+  default     = "arn:aws:acm:us-east-1:968246069280:certificate/8c7c4d6b-5009-42d8-afe5-4c913d23a7f9"
+}
