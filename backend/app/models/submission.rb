9fc8165d36ac18c2ffa3dcca54e8831829afed1c
Eugene Doan <U-DON@users.noreply.github.com>
2018-01-23 12:42:50 -0500

    Submission approval/denial and notes
    closes #56 
    closes #55

--- backend/app/models/submission.rb
+++ backend/app/models/submission.rb
@@ -3,5 +3,5 @@
 class Submission < ApplicationRecord
   belongs_to :customer
   enum status: [ :requested, :submitted, :approved, :denied ]
-  after_initialize { self.status = :requested }
+  after_initialize { self.status = :requested if self.new_record? }
 end
