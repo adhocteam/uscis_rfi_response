246aa3795a764ca753c86474a930252d472bdaaf
Ryan Nagle <rmnagle@gmail.com>
2018-01-23 14:57:59 -0600

    add cloudfront config for the backend app

--- ops/terraform/dev/frontend/main.tf
+++ ops/terraform/dev/frontend/main.tf
@@ -28,3 +28,42 @@ resource "aws_s3_bucket" "uscis_frontend" {
     Name = "${var.bucket_name}"
   }
 }
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.uscis_frontend.bucket_domain_name}"
+    origin_id   = "uscis-frontend-origin"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  aliases = ["uscis-rfds.adhocteam.us"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "uscis-frontend-origin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  tags {
+    Name = "${var.bucket_name}"
+  }
+
+  acm_certificate_arn = "${var.ssl_cert_id}"
+}
