632ea503db60faa3428d1307c2213a74ceb91685
Bill Mill <bill@billmill.org>
2018-01-23 16:00:09 -0500

    use a data volume for a bundler cache
    closes #72

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,4 +7,11 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+# bundle install if it needs to be done. Note that we have set /bundle as the
+# bundle cache in the Dockerfile, and added /bundle as a data volume in the 
+# docker-compose file
+# https://engineering.adwerx.com/rails-on-docker-compose-7e2cf235fa0e
+echo "bundle path: $BUNDLE_PATH"
+bundle check || bundle install
+
 exec bundle exec "$@"
