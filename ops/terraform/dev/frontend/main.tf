8e78e92afcc452b04f68d01acb801e75f4d4e11c
Ryan Nagle <rmnagle@gmail.com>
2018-01-23 15:46:22 -0600

    use the s3 bucket's website endpoint for the cloudfront config

--- ops/terraform/dev/frontend/main.tf
+++ ops/terraform/dev/frontend/main.tf
@@ -31,8 +31,17 @@ resource "aws_s3_bucket" "uscis_frontend" {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name = "${aws_s3_bucket.uscis_frontend.bucket_domain_name}"
+    domain_name = "${aws_s3_bucket.uscis_frontend.website_endpoint}"
     origin_id   = "uscis-frontend-origin"
+
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_keepalive_timeout = 5
+      origin_protocol_policy   = "http-only"
+      origin_read_timeout      = 30
+      origin_ssl_protocols     = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
   }
 
   enabled             = true
---
+++ ops/terraform/dev/frontend/outputs.tf
@@ -0,0 +1,3 @@
+output "website_endpoint" {
+  value = "${aws_s3_bucket.uscis_frontend.website_endpoint}"
+}
