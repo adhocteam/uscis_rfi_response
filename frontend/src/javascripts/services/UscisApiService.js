d4117846faf6400467211971321c797f5b6c5a6d
Louis Fettet <louisfettet@gmail.com>
2018-01-24 16:33:25 -0500

    Implement log out functionality.  Write tests for log in and log out.  Move history into services.

--- frontend/src/javascripts/services/UscisApiService.js
+++ frontend/src/javascripts/services/UscisApiService.js
@@ -1,4 +1,4 @@
-import history from "../history";
+import history from "./history";
 
 const authedRequest = (url, settings, error) =>
   fetch(url, {
@@ -8,9 +8,9 @@ const authedRequest = (url, settings, error) =>
       "access-token": sessionStorage.getItem("token"),
       client: sessionStorage.getItem("client"),
       uid: sessionStorage.getItem("uid"),
-      expiry: sessionStorage.getItem("expiry")
+      expiry: sessionStorage.getItem("expiry"),
     },
-    ...settings
+    ...settings,
   }).then(resp => {
     if (resp.ok) {
       let token = resp.headers.get("access-token");
@@ -38,13 +38,13 @@ const UscisApiService = {
       method: "POST",
       headers: {
         Accept: "application/json",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         user_id: user_id,
         image_name: image_name,
-        image_type: image_type
-      })
+        image_type: image_type,
+      }),
     }).then(resp => {
       if (resp.status === 200) {
         return resp.json();
@@ -71,12 +71,12 @@ const UscisApiService = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json"
+        Accept: "application/json",
       },
       body: JSON.stringify({
         email: email,
-        password: password
-      })
+        password: password,
+      }),
     }).then(resp => {
       if (resp.ok) {
         sessionStorage.setItem("token", resp.headers.get("access-token"));
@@ -95,10 +95,10 @@ const UscisApiService = {
       `/submissions/${id}`,
       {
         method: "PUT",
-        body: JSON.stringify({ notes, status })
+        body: JSON.stringify({ notes, status }),
       },
       `Failed to update submission ${id}.`
-    )
+    ),
 };
 
 export default UscisApiService;
