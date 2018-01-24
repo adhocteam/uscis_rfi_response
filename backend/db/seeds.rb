072784fe5e639e57c6aaeb7224c53c0dc3079697
Bill Mill <bill@billmill.org>
2018-01-24 09:32:46 -0500

    add a db seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# to run:
+# docker-compose run backend rake db:seed
+10.times do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      street1: Faker::Address.street_address,
+      street2: Faker::Address.secondary_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      zip: Faker::Address.zip,
+      dob: Faker::Date.between(18.years.ago, 80.years.ago)
+    )
+
+    submission = Submission.new(
+      timestamp: Faker::Time.between(Time.current - 1.day, Time.current),
+      uri: Faker::Internet.url,
+      notes: Faker::Lorem.sentence
+    )
+
+    submission.customer = customer
+
+    submission.save
+end
