870ed862b4804c323e6e1d16c8f12fde32ee019c
Ryan Nagle <rmnagle@gmail.com>
2018-01-24 20:42:39 -0600

    Revert "raise 404 on root"
    This reverts commit 5426be2750f7bcd36102fd27db790835ea9d43ae.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-
-  def not_found
-    raise ActionController::RoutingError.new('Not Found')
-  end
 end
