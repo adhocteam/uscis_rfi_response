eaeb17fb5b4e7eba084d03225f4edcb173c419ff
Louis Fettet <LouisFettet@gmail.com>
2018-01-25 07:17:21 -0500

    Header Navigation Links and Name Plate (#83)
    * First attempt at adding nav links.  Update header.
    
    * Changed log out functionality so that it occurs nearly immediately upon navigating to /logout.  Added dynamic nav links based on whether the user is logged in or out. Made style changes to header.  Added a nameplate under the header when the user is logged in.  Created small helper script for dealing with session storage.  Added snapshot tests for nav links and nameplate.

--- frontend/src/javascripts/components/App.js
+++ frontend/src/javascripts/components/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { Router, Link, Route, Switch } from "react-router-dom";
 
 import history from "../services/history";
+import NavLinks from "./NavLinks";
+import NamePlate from "./NamePlate";
 import AdminPage from "./AdminPage";
 import LoginPage from "./LoginPage";
 import LogoutPage from "./LogoutPage";
@@ -11,20 +13,27 @@ export default function App() {
   return (
     <Router history={history}>
       <div>
-        <div className="ds-u-fill--primary-darkest">
-          <div className="ds-l-container ds-u-md-padding-top--1">
+        <header className="ds-u-fill--primary-darkest">
+          <div className="ds-l-container">
             <div className="ds-l-row">
-              <div className="ds-l-col">
+              <div className="ds-l-col--12 ds-l-md-col--4">
                 <h1
                   id="main"
-                  className="ds-u-serif ds-u-color--white ds-u-font-size--h2 ds-u-md-font-size--h1 ds-u-font-weight--normal ds-u-margin-top--3 ds-u-margin-bottom--3"
+                  className="ds-u-margin--0 ds-u-margin-top--1 ds-u-margin-bottom--1"
                 >
-                  USCIS RFDS Front-End
+                  <Link
+                    to="/"
+                    className="ds-u-color--white ds-u-font-size--h3 ds-u-font-weight--bold"
+                  >
+                    USCIS RFDS Front-End
+                  </Link>
                 </h1>
               </div>
+              <NavLinks />
             </div>
           </div>
-        </div>
+          <NamePlate />
+        </header>
         <Switch>
           <Route path="/login" exact component={LoginPage} />
           <Route path="/logout" exact component={LogoutPage} />
