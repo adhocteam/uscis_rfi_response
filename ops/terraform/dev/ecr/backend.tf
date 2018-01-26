b8c129734274d31f4e3342478bb0147fd5dbad0f
Ryan Nagle <rmnagle@gmail.com>
2018-01-26 13:26:58 -0600

    move kms_key_id out of tf backend definitions

--- ops/terraform/dev/ecr/backend.tf
+++ ops/terraform/dev/ecr/backend.tf
@@ -5,6 +5,5 @@ terraform {
     region         = "us-east-1"
     dynamodb_table = "uscis-tf-table"
     encrypt        = "1"
-    kms_key_id     = "arn:aws:kms:us-east-1:968246069280:key/176febca-5a61-4a48-9bb9-79cc4e6d8216"
   }
 }
